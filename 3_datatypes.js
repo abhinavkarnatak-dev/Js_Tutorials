d5dc7834e69a307df57a44e57ac2a461726c8756
Abhinav Karnatak <167810640+abhinavkarnatak-dev@users.noreply.github.com>
2024-06-09 16:50:10 +0000

    learned datatypes in js

---
+++ 3_datatypes.js
@@ -0,0 +1,15 @@
+"use strict"; // Treat all javascript code as newer version
+
+// alert("Hello"); // Works in console/browser, not in nodejs
+
+let name = "Abhinav" // String datatype
+let age = 19 // Number
+let isLoggedIn = false // Boolean (true or false)
+let state = null // standalone value
+let undefinedState; // undefined/value not assigned
+// BigInt for large number
+// symbol for uniqueness
+
+// object (null is a typeof object)
+
+console.log(typeof age); // Tells the type of datatype
