f8a2073e762742469dd1404dcd373f08c15eb124
Abhinav Karnatak <167810640+abhinavkarnatak-dev@users.noreply.github.com>
2024-06-13 18:08:54 +0000

    learned iterations in js

---
+++ 17_iterations.js
@@ -0,0 +1,24 @@
+// Loops or iterations
+
+// for loop
+for (let i = 1; i <= 10; i++) {
+    const element = i;
+    console.log(element);
+}
+
+console.log("---------------------------------------------");
+
+//while loop
+let i = 1
+while (i<=10) {
+    console.log(i);
+    i++
+}
+
+console.log("---------------------------------------------");
+
+let j = 1
+do {
+    console.log(j);
+    j++
+} while (j<=10);
