0a1cda864082e018c7a82d89b3b9a54952600c16
Abhinav Karnatak <167810640+abhinavkarnatak-dev@users.noreply.github.com>
2024-06-13 18:45:46 +0000

    learned loops for arrays in js

--- 18_arrayLoops.js
+++ 18_arrayLoops.js
@@ -1,12 +1,34 @@
-// for-of loop
+// for-of loop (doesn't work on objects)
 const arr = [1, 2, 3, 4, 5]
 for (const num of arr) {
     console.log(num);
 }
-
-// Maps - datatype (key-value pair, no duplicate values)
+// Maps - datatype (key-value pair, unique values)
 const map = new Map()
 map.set('IN', "India")
 map.set('USA', "United States of America")
 map.set('FR', "France")
 console.log(map);
+for (const [key, value] of map) {
+    console.log(key, ':-', value);
+}
+
+// for-in loop (works on objects)
+const myObject = {
+    js: 'javascript',
+    cpp: 'C++',
+    rb: 'ruby',
+    swift: 'swift by apple'
+}
+for (const key in myObject) {
+    console.log(key, ':- ', myObject[key]);
+}
+
+// for-each loop
+const coding = ["js", "ruby", "java", "python", "cpp"]
+coding.forEach( function (item) { // Callback function has no name
+    console.log(item);
+} )
+coding.forEach( (item, index, arrList) => {
+    console.log(item, index, arrList);
+})
